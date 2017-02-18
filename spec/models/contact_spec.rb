f3a3042f0ff7bd02ae593f7f552008e5a8d0d21b
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2017-02-18 15:33:38 +0900

    3章演習課題完了

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -38,12 +38,95 @@ describe Contact do
                          lastname: 'Tester',
                          email: 'tester@example.com'
     )
-    expect(contact.errors[:email]).to include("has already been token")
+
+    contact.valid?
+    expect(contact.errors[:email]).to include("has already been taken")
   end
 
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address"
+  it "is invalid with a duplicate email address" do
+    contact = Contact.create(
+                         firstname: 'Joe',
+                         lastname: 'Tester',
+                         email: 'tester@example.com'
+    )
+    contact.phones.create(
+                      phone_type: 'home',
+                      phone: '785-555-1234'
+    )
+    mobile_phone = contact.phones.build(
+                                     phone_type: 'mobile',
+                                     phone: '785-555-1234'
+    )
+
+    mobile_phone.valid?
+    expect(mobile_phone.errors[:phone]).to include('has already been taken')
+  end
 
   # 連絡先のフルネームを文字列として返すこと
-  it "returns a contact's full name as a string"
+  it "returns a contact's full name as a string" do
+    contact = Contact.create(
+        firstname: 'Joe',
+        lastname: 'Tester',
+        email: 'tester@example.com'
+    )
+    contact.phones.create(
+        phone_type: 'home',
+        phone: '785-555-1234'
+    )
+
+    other_contact = Contact.new
+    other_phone = other_contact.phones.build(
+                                            phone_type: 'home',
+                                            phone: '785-555-1234'
+    )
+
+    expect(other_phone).to be_valid
+  end
+
+  # 連絡先のフルネームを文字列として返すこと
+  it "returns a contact's full name as a string" do
+    contact = Contact.new(
+                         firstname: 'John',
+                         lastname: 'Doe',
+                         email: 'tester@example.com'
+    )
+
+    expect(contact.name).to eq 'John Doe'
+  end
+
+  describe "filter last name by letter" do
+    before :each do
+      @smith = Contact.create(
+          firstname: 'John',
+          lastname: 'Smith',
+          email: 'jsmith@example.com'
+      )
+      @jones = Contact.create(
+          firstname: 'Tim',
+          lastname: 'Jones',
+          email: 'tjones@example.com'
+      )
+      @johnson = Contact.create(
+          firstname: 'John',
+          lastname: 'Johnson',
+          email: 'jjohnson@example.com'
+      )
+    end
+    # マッチする文字の場合
+    context "matching letters" do
+      # マッチした結果をソート済みの配列として返すこと
+      it "returns sorted array of results that match" do
+        expect(Contact.by_letter("J")).to eq [@johnson, @jones]
+      end
+    end
+
+    # マッチしない文字の場合
+    context "non-matching letters" do
+      # マッチしなかったものは結果に含まれていないこと
+      it "returns sorted array of results that match" do
+        expect(Contact.by_letter("J")).not_to include @smith
+      end
+    end
+  end
 end
