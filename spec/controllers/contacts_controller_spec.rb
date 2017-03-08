0954bfc696c2efcaacd21aa12f6ff8ab977d3548
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2017-03-08 09:58:06 +0900

    5章のPOST #createのspecまで完了

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -6,17 +6,35 @@ describe ContactsController do
     # params[:letter]がある場合
     context 'with params[:letter]' do
       # 指定された文字が始まる連絡先を配列にまとめること
-      it "populates an array of contacts starting with the letter"
+      it "populates an array of contacts starting with the letter" do
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        get :index, letter: 'S'
+        expect(assigns(:contacts)).to match_array([smith])
+      end
+
       # :indexテンプレートを表示すること
-      it "renders the :index template"
+      it "renders the :index template" do
+        get :index, letter: 'S'
+        expect(response).to render_template :index
+      end
     end
 
     # params[:letter]がない場合
     context 'without params[:letter]' do
       # すべての連絡先を配列にまとめること
-      it "populates an array of all contacts"
+      it "populates an array of all contacts" do
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        get :index
+        expect(assigns(:contacts)).to match_array([smith, jones])
+      end
+
       # :indexテンプレートを表示すること
-      it "renders the :index template"
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
+      end
     end
   end
 
@@ -37,37 +55,77 @@ describe ContactsController do
 
   describe 'GET #new' do
     # @contactに新しい連絡先を割り当てること
-    it "assigns a new Contact to @contact"
+    it "assigns a new Contact to @contact" do
+      get :new
+      expect(assigns(:contact)).to be_a_new(Contact)
+    end
+
     # :newテンプレートを表示すること
-    it "renders the :new template"
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
   end
 
   describe 'GET #edit' do
     # @contactに要求された連絡先を割り当てること
-    it "assigns the requested contact to @contact"
+    it "assigns the requested contact to @contact" do
+      contact = create(:contact)
+      get :edit, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
+
     # :editテンプレートを表示すること
-    it "renders the :edit template"
+    it "renders the :edit template" do
+      contact = create(:contact)
+      get :edit, id: contact
+      expect(response).to render_template :edit
+    end
   end
 
   describe 'POST #create' do
+    before :each do
+      @phones = [
+          attributes_for(:phone),
+          attributes_for(:phone),
+          attributes_for(:phone)
+      ]
+    end
+
     # 有効な属性の場合
     context "with valid attributes" do
       # データベースに新しい連絡先を保存すること
-      it "saves the new contact in the database"
+      it "saves the new contact in the database" do
+        expect{
+          post :create, contact: attributes_for(:contact, phones_attributes: @phones)
+        }.to change(Contact, :count).by(1)
+      end
+
       # contacts#showにリダイレクトすること
-      it "redirects to contacts#show"
+      it "redirects to contacts#show" do
+        post :create, contact: attributes_for(:contact, phones_attributes: @phones)
+        expect(response).to redirect_to contact_path(assigns[:contact])
+      end
     end
 
     # 無効な属性の場合
     context "with invalid attributes" do
       # データベースに新しい連絡先を保存しないこと
-      it "updates the contact in the database"
+      it "updates the contact in the database" do
+        expect{
+          post :create, contact: attributes_for(:invalid_contact)
+        }.not_to change(Contact, :count)
+      end
+
       # :newテンプレートを再表示すること
-      it "re-renders the :new template"
+      it "re-renders the :new template" do
+        post :create, contact: attributes_for(:invalid_contact)
+        expect(response).to render_template :new
+      end
     end
   end
 
-  describe 'PATH #update' do
+  describe 'PATCH #update' do
     # 有効な属性の場合
     context "with valid attributes" do
       # データベースの連絡先を更新すること
